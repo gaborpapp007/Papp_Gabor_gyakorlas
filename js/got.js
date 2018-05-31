35ddec39a85286662480a224cbd9c62066a78268
Gabor Papp <pappgabesz007@gmail.com>
2018-05-31 10:31:52 +0200

    rendezés név szerint

--- js/got.js
+++ js/got.js
@@ -22,7 +22,10 @@ function successAjax(xhttp) {
       A userDatas NEM GLOBÁLIS változó, ne is tegyétek ki globálisra. Azaz TILOS!
       Ha valemelyik függvényeteknek kell, akkor paraméterként adjátok át.
     */
-    console.log(livingCharacters(userDatas));
+    //az élők;
+    var alive = livingCharacters(userDatas);
+    var sorted = sortByName(alive);
+    console.log(sorted);
 }
 
 function livingCharacters(characters) {
@@ -33,6 +36,42 @@ function livingCharacters(characters) {
         }
     }
     return live;
+}
+
+function sortByName(characters) {
+
+    //javított bubis
+    var i = characters.length - 1;
+    var tmp;
+    var swap = false;
+    do {
+        swap = false;
+        for (let j = 0; j < i; j++) {
+            if (characters[j].name > characters[j + 1].name) {
+                tmp = characters[j];
+                characters[j] = characters[j + 1];
+                characters[j + 1] = tmp;
+                swap = true;
+            }
+        }
+        i--;
+    } while (i >= 0 && swap)
+    return characters;
+
+
+
+    /*var rendezetTomb = characters;
+    for (var i = rendezetTomb.length; i > 0; i--) {
+        for (var j = 0; j < i - 1; j++) {
+            if (rendezetTomb[j].age > rendezetTomb[j + 1].age) {
+                var tmp = rendezetTomb[j];
+                rendezetTomb[j] = rendezetTomb[j + 1];
+                rendezetTomb[j + 1] = tmp;
+            }
+        }
+    }
+    return rendezetTomb;*/
+
 }
 // Írd be a json fileod nevét/útvonalát úgy, ahogy nálad van
 getData('/json/characters.json', successAjax);
